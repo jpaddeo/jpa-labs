a0ea857d9bf20b19e3280f385d3ff2dff5db205a
Juan Pablo Addeo <jpaddeo@gmail.com>
2021-09-09 22:05:42 -0300

    Chatbox avances

--- chatbot/src/bot/config.js
+++ chatbot/src/bot/config.js
@@ -1,6 +1,7 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
 
 import LearningOptions from '../components/LearningOptions/LearningOptions';
+import LinkList from '../components/LinkList/LinkList';
 
 const config = {
   botName: 'LearningBot',
@@ -21,6 +22,29 @@ const config = {
     {
       widgetName: 'learningOptions',
       widgetFunc: (props) => <LearningOptions {...props} />,
+    },
+    {
+      widgetName: 'javascriptLinks',
+      widgetFunc: (props) => <LinkList {...props} />,
+      props: {
+        options: [
+          {
+            text: 'Introduction to JS',
+            url: 'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/',
+            id: 1,
+          },
+          {
+            text: 'Mozilla JS Guide',
+            url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide',
+            id: 2,
+          },
+          {
+            text: 'Frontend Masters',
+            url: 'https://frontendmasters.com',
+            id: 3,
+          },
+        ],
+      },
     },
   ],
 };
--- chatbot/src/components/LinkList/LinkList.jsx
+++ chatbot/src/components/LinkList/LinkList.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 import './LinkList.css';
 
-const LinkList = () => {
-  return <div></div>;
+const LinkList = ({ options = [] }) => {
+  const linksMarkup = options.map(({ id, url, text }) => (
+    <li key={id} className='link-list-item'>
+      <a
+        href={url}
+        target='_blank'
+        rel='noopener noreferrer'
+        className='link-list-item-url'
+      >
+        {text}
+      </a>
+    </li>
+  ));
+  return <ul className='link-list'>{linksMarkup}</ul>;
 };
 
 export default LinkList;
