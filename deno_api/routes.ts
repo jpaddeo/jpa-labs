233ad01e5fcda05d59b18019163ac7377ca3e0de
jpaddeo <jpaddeo@gmail.com>
2022-02-16 23:18:29 -0300

    Adding routes.

--- deno_api/routes.ts
+++ deno_api/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
+import { addTodo } from './controllers/todos.ts';
 
 const router = new Router();
 
 // TODO: routes definition
+router.post('/api/todos', addTodo);
 
 export default router;
